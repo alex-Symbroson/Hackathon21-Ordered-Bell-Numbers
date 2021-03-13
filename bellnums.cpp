261f624a2786bf4b360dde65035503d85a43bfec
alex-Symbroson <alex.symbroson@gmail.com>
2021-03-13 15:32:11 +0100

    minor fix

--- bellnums.cpp
+++ bellnums.cpp
@@ -4,16 +4,16 @@
 
 using namespace std;
 
-mpz_class *num0, *num1;
 
 void bell_number(int n)
 {
-	mpz_class *num;
-	num0[1] = 1;
+	mpz_class _num0[n+1], _num1[n+1], *num;
+	mpz_class *num0 = _num0, *num1 = _num1;
+	if(n) num0[1] = 1;
 	
 	cout << "[" << (n > 1 ? "1, 1" : n ? "1" : "");
 	
-	for(int i=0; i<n-1; i++)
+	for(int i=0; i<n-2; i++)
 	{
 		for(int k=1; k<i+2; k++)
 			num1[k] = k * (num0[k-1] + num0[k]);
@@ -33,8 +33,6 @@ int main (int argc, const char** argv)
 {
 	int n = 10;
 	if(argc > 1) n = stoi(argv[1]);
-	mpz_class _num0[n+1], _num1[n+1];
-	num0=_num0; num1=_num1;
 	bell_number(n);
 }
 
